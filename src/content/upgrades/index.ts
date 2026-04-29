e9525d089759c50c6a6f13cb5ad3b0b6142050e6
mariosumali <swimsuper12@gmail.com>
2026-04-29 12:34:27 -0700

    feat(upgrades): register relic item pools

--- src/content/upgrades/index.ts
+++ src/content/upgrades/index.ts
@@ -3,6 +3,8 @@ import { initShopServiceContent } from './meta/registry';
 import { LANDMARK_UPGRADES } from './landmark';
 import { LANDMARK_EXPANSION_UPGRADES } from './landmark_expansion';
 import { MORE_LANDMARK_UPGRADES } from './landmark_more';
+import { RELIC_UPGRADES } from './relics';
+import { BAUBLE_UPGRADES } from './baubles';
 
 let initialized = false;
 
@@ -13,6 +15,8 @@ export function initUpgradeContent(): void {
     ...LANDMARK_UPGRADES,
     ...LANDMARK_EXPANSION_UPGRADES,
     ...MORE_LANDMARK_UPGRADES,
+    ...RELIC_UPGRADES,
+    ...BAUBLE_UPGRADES,
   ]);
   initShopServiceContent();
 }
